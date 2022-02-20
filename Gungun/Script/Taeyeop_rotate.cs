df4eb2eebd70ebe22ce64a9189d189e82873434c
Goen Hee Han <gksrjsgml13@naver.com>
2022-02-20 19:15:40 +0900

    Taeyeop rotate reset
    태엽 Rotate값 초기화 수정

--- Gungun/Script/Taeyeop_rotate.cs
+++ Gungun/Script/Taeyeop_rotate.cs
@@ -8,6 +8,8 @@ public class Taeyeop_rotate : MonoBehaviour
     public GameObject taeyeop_body;
     public GameObject Handle;
     public GameObject my;
+    //public GameObject reset;
+    public Transform SnapGraphics;
 
     public Quaternion origin_rotation;
     public Quaternion finish_rotation;
@@ -17,18 +19,28 @@ public class Taeyeop_rotate : MonoBehaviour
     {
         GFX_local_original = my.transform.localRotation;
     }
+    private void OnEnable()
+    {
+        gameObject.transform.localEulerAngles = Vector3.zero;
+    }
     // Update is called once per frame
     void Update()
     {
         //Vector3 v = gameObject.transform.rotation.ToEulerAngles();
         //print(v);
-        print("È¸Àü°ª : "  + my.transform.localRotation.eulerAngles.y);
         if (my.transform.localRotation.eulerAngles.y >= 180)
         {
             //Handle.SetActive(false);
             my.transform.localRotation = GFX_local_original;
             taeyeop_body.SetActive(true);
+            //reset.SetActive(true);
+            //my.transform.localEulerAngles = new Vector3(my.transform.localEulerAngles.x, 0, my.transform.localEulerAngles.z);
+            //SnapGraphics.localEulerAngles = new Vector3(SnapGraphics.localEulerAngles.x, 0, SnapGraphics.localEulerAngles.z);
+            gameObject.transform.localEulerAngles = Vector3.zero;
+            SnapGraphics.localEulerAngles = Vector3.zero;
+            Handle.SetActive(false);
             taeyeop_main.SetActive(false);
+            gameObject.SetActive(false);
         }
     }
 }
