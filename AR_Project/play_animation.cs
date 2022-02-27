31e44ef4477b68ea72593526d096b97cf8527008
Goen Hee Han <gksrjsgml13@naver.com>
2022-02-28 00:19:37 +0900

    original update
    track file update

--- AR_Project/play_animation.cs
+++ AR_Project/play_animation.cs
@@ -5,32 +5,29 @@ using UnityEngine;
 public class play_animation : MonoBehaviour
 {
     public Animator claw;
-    bool push;
     bool duplicate;
     float speed = 4f;
-    // Start is called before the first frame update
+
     void Start()
     {
         claw.Play("Base Layer.Armature|Stay", 0, 0.25f);
-        push = true;
         duplicate = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             if (duplicate)
-                push = true;
+                StartCoroutine(Duplicate_timer());
         }
+    }
 
-        if (push)
-        {
-            duplicate = false;
-            claw.Play("Base Layer.Armature|action", 0, 0.25f);
-            push = false;
-            duplicate = true;
-        }
+    IEnumerator Duplicate_timer()
+    {
+        duplicate = false;
+        claw.Play("Base Layer.Armature|action", 0, 0.25f);
+        yield return new WaitForSeconds(5.0f);
+        duplicate = true;
     }
 }
